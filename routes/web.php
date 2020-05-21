40308273cf4e594d4924bb1e08049b73dbbc522b
LeoSantosRS <bigsoftsbrasil@gmail.com>
2020-05-21 19:54:57 -0300

    introdução a resources e di

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Controle de CRUD
+
+// Route Controller resources
+Route::resource('products', 'ProductController');
+
+/*
+// Controle de CRUD 
 //create
-Route::get('products/create','ProductController@create')->name('products.create'); //criar modelo
-Route::post('products','ProductController@store')->name('products.store'); //cadastrar registro
+Route::get('products/create','ProductController@create')->name('products.create'); //criar
+Route::post('products','ProductController@store')->name('products.store'); //registrar
 //read
-Route::get('products/{id}', 'ProductController@show')->name('products.show');//exibir por id
+Route::get('products/{id}', 'ProductController@show')->name('products.show');//exibir
 Route::get('products', 'ProductController@index')->name('products.index');//listar
 //update
 Route::get('products/{id}/edit','ProductController@edit')->name('products.edit');//editar modelo
 Route::put('products/{id}/edit','ProductController@update')->name('products.update');//editar registro
 //delete
-routeDB::delete('products/{id', 'ProductController@destroy')->name('products.destroy');//deletar registro
-
+RouteDB::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');//deletar registro
+*/
 
